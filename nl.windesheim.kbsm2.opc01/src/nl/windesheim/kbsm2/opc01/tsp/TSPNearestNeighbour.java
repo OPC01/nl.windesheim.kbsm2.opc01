19e2edb5955ba9cb5f61152bb9bb08e296080f59
lurtze1 <lurtze1@gmail.com>
2015-04-22 14:24:00 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Options.java

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPNearestNeighbour.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPNearestNeighbour.java
@@ -21,29 +21,27 @@ public class TSPNearestNeighbour
 
     public TSPNearestNeighbour()
     {
-
+        stack = new Stack<Integer>();
     }
 
     public void tsp(double adjacencyMatrix[][])
     {
         ArrayList<Integer> path = new ArrayList<Integer>();
         path.add(1);
-        Stack<Integer> stack = new Stack<Integer>();
-        int numberOfNodes = adjacencyMatrix[1].length - 1;
-        int[] visited = new int[numberOfNodes + 1];
-        visited[1] = 1;
-        stack.push(1);
+        int numberOfNodes = adjacencyMatrix[0].length;
+        int[] visited = new int[numberOfNodes];
+        visited[0] = 1;
+        stack.push(0);
         int element, dst = 0, i;
         double min = Double.MAX_VALUE;
         boolean minFlag = false;
-        System.out.print(1 + "\t");
 
         while (!stack.isEmpty())
         {
             element = stack.peek();
-            i = 1;
+            i = 0;
             min = Double.MAX_VALUE;
-            while (i <= numberOfNodes)
+            while (i < numberOfNodes)
             {
                 if (adjacencyMatrix[element][i] > 1 && visited[i] == 0)
                 {
@@ -53,6 +51,7 @@ public class TSPNearestNeighbour
                         dst = i;
                         minFlag = true;
                     }
+
                 }
                 i++;
             }
@@ -67,18 +66,8 @@ public class TSPNearestNeighbour
             }
             stack.pop();
         }
-        /**
-         * *this.path = path; double routelength = 0; int originPacketCounter;
-         * int destinationPacketCounter; for (destinationPacketCounter = 1;
-         * destinationPacketCounter < path.size(); destinationPacketCounter++) {
-         * originPacketCounter = destinationPacketCounter - 1; int originPacket
-         * = path.get(originPacketCounter); int destinationPacket =
-         * path.get(destinationPacketCounter); routelength = routelength +
-         * adjacencyMatrix[originPacket][destinationPacket];
-         *
-         * }
-         * System.out.println(routelength);
-         */
 
+        this.path = path;
     }
+
 }
