1fb102193e03487923c5b58da37c4892fccdbf5a
DeeJayOsti <DeeJayOsti@145.44.76.237>
2015-04-28 13:58:26 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPNearestNeighbour.java

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Main.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Main.java
@@ -1,35 +1,43 @@
 package nl.windesheim.kbsm2.opc01.tsp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
-import javax.swing.JFrame;
+import java.util.*;
+import javax.swing.*;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        DistanceMap z = new DistanceMap();
-        Packet f = new Packet(0, 0);
-        Packet a = new Packet(10, 0);
-        Packet b = new Packet(20, 0);
-        Packet c = new Packet(30, 0);
-        Packet d = new Packet(0, 5);
-        Packet e = new Packet(1, 30);
-        ArrayList<Packet> list = new ArrayList<Packet>();
-        list.add(f);
-        list.add(a);
-        list.add(b);
-        list.add(c);
-        list.add(d);
-        list.add(e);
-        z.createMatrix(list);
-        TSPNearestNeighbour x = new TSPNearestNeighbour();
-        x.tsp(z.getMap());
-        System.out.println(Arrays.deepToString(z.getMap()));
-        x.calculatePathLength(z.getMap(), list);
-        System.out.println(x.getLengthOfPath());
-        ArrayList<Integer> y = x.getPath();        
+        /*
+         DistanceMap z = new DistanceMap();
+         Packet f = new Packet(0, 0);
+         Packet a = new Packet(10, 0);
+         Packet b = new Packet(20, 0);
+         Packet c = new Packet(30, 0);
+         Packet d = new Packet(0, 5);
+         Packet e = new Packet(1, 30);
+         ArrayList<Packet> list = new ArrayList<Packet>();
+         list.add(f);
+         list.add(a);
+         list.add(b);
+         list.add(c);
+         list.add(d);
+         list.add(e);
+         z.createMatrix(list);
+         TSPNearestNeighbour x = new TSPNearestNeighbour();
+         x.tsp(z.getMap());
+         System.out.println(Arrays.deepToString(z.getMap()));
+         x.calculatePathLength(z.getMap(), list);
+         System.out.println(x.getLengthOfPath());
+         ArrayList<Integer> y = x.getPath();
+         */
+
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+        for (int i = 0; i < 6; ++i)
+        {
+            lst.add(i);
+        }
+        ArrayList<Integer> route = new ArrayList<Integer>();
+        TSPBruteForce.bruteForce(route, lst);
     }
 }
