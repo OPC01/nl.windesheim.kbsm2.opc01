22b16a8cd8498261f5ce2110878a8e0a20c3e9e3
DeeJayOsti <jpjvangelder@gmail.com>
2015-04-30 15:22:41 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/Main.java

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPBruteForce.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPBruteForce.java
@@ -1,7 +1,49 @@
 package nl.windesheim.kbsm2.opc01.tsp;
 
+import java.util.*;
 
 public class TSPBruteForce
 {
-    
+    private static ArrayList<Integer> bestRoute;
+
+    public static void bruteForce(ArrayList<Integer> r, ArrayList<Integer> citiesNotInRoute)
+    {
+        if (!citiesNotInRoute.isEmpty())
+        {
+            for (int i = 0; i < citiesNotInRoute.size(); i++)
+            {
+                Integer justRemoved = citiesNotInRoute.remove(0);
+                ArrayList<Integer> newRoute = (ArrayList<Integer>) r.clone();
+                newRoute.add(justRemoved);
+
+                bruteForce(newRoute, citiesNotInRoute);
+                citiesNotInRoute.add(justRemoved);
+            }
+        }
+        else
+        {
+            if (isBestRoute(r))
+            {
+                bestRoute = r;
+            }
+        }
+
+    }
+
+    private static boolean isBestRoute(ArrayList<Integer> r)
+    {
+        System.out.println(r.toString());
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+        for (int i = 0; i < 6; ++i)
+        {
+            lst.add(i);
+        }
+        ArrayList<Integer> route = new ArrayList<Integer>();
+        bruteForce(route, lst);
+    }
 }
