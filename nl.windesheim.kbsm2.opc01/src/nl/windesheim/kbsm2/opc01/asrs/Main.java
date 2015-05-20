b3094c8ffd04e48bf0f6a92d88bdeff6cce7fe1e
Robnibourg93 <robnibourg@hotmail.nl>
2015-05-20 12:48:59 +0200

    order invoegen in database vanuit xml

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/asrs/Main.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/asrs/Main.java
@@ -1,15 +1,6 @@
 package nl.windesheim.kbsm2.opc01.asrs;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-
 
 public class Main extends JFrame {
 
