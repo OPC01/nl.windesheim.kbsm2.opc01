0b4cd8e63ed5cbbcaa7dda8b09f47edba9b36cd2
Jeffrey <Jeffrey@Lurtze1>
2015-05-24 14:48:04 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/SimulationResults.java

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPNearestNeighbour.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/tsp/TSPNearestNeighbour.java
@@ -2,21 +2,18 @@ package nl.windesheim.kbsm2.opc01.tsp;
 
 import java.util.*;
 
-public class TSPNearestNeighbour
-{
+public class TSPNearestNeighbour {
 
     private int numberOfNodes;
     private Stack<Integer> stack;
     private ArrayList<Integer> path;
     private double lengthOfPath;
 
-    public TSPNearestNeighbour()
-    {
+    public TSPNearestNeighbour() {
         stack = new Stack<Integer>();
     }
 
-    public void tsp(double[][] adjacencyMatrix)
-    {
+    public void tsp(double[][] adjacencyMatrix) {
         ArrayList<Integer> path = new ArrayList<Integer>();
         path.add(0);
         int numberOfNodes = adjacencyMatrix[0].length;
@@ -26,17 +23,13 @@ public class TSPNearestNeighbour
         int element, dst = 0, i;
         double min = Double.MAX_VALUE;
         boolean minFlag = false;
-        while (!stack.isEmpty())
-        {
+        while (!stack.isEmpty()) {
             element = stack.peek();
             i = 0;
             min = Double.MAX_VALUE;
-            while (i < numberOfNodes)
-            {
-                if (adjacencyMatrix[element][i] > 0.1 && visited[i] == 0)
-                {
-                    if (min > adjacencyMatrix[element][i])
-                    {
+            while (i < numberOfNodes) {
+                if (adjacencyMatrix[element][i] > 0.1 && visited[i] == 0) {
+                    if (min > adjacencyMatrix[element][i]) {
                         min = adjacencyMatrix[element][i];
                         dst = i;
                         minFlag = true;
@@ -45,8 +38,7 @@ public class TSPNearestNeighbour
                 }
                 i++;
             }
-            if (minFlag)
-            {
+            if (minFlag) {
                 path.add(dst);
                 visited[dst] = 1;
                 stack.push(dst);
@@ -60,51 +52,41 @@ public class TSPNearestNeighbour
         this.calculatePathLength(adjacencyMatrix);
     }
 
-    public int getNumberOfNodes()
-    {
+    public int getNumberOfNodes() {
         return numberOfNodes;
     }
 
-    public void setNumberOfNodes(int numberOfNodes)
-    {
+    public void setNumberOfNodes(int numberOfNodes) {
         this.numberOfNodes = numberOfNodes;
     }
 
-    public Stack<Integer> getStack()
-    {
+    public Stack<Integer> getStack() {
         return stack;
     }
 
-    public void setStack(Stack<Integer> stack)
-    {
+    public void setStack(Stack<Integer> stack) {
         this.stack = stack;
     }
 
-    public ArrayList<Integer> getPath()
-    {
+    public ArrayList<Integer> getPath() {
         return path;
     }
 
-    public void setPath(ArrayList<Integer> path)
-    {
+    public void setPath(ArrayList<Integer> path) {
         this.path = path;
     }
 
-    public double getLengthOfPath()
-    {
+    public double getLengthOfPath() {
         return lengthOfPath;
     }
 
-    public void setLengthOfPath(double lengthOfPath)
-    {
+    public void setLengthOfPath(double lengthOfPath) {
         this.lengthOfPath = lengthOfPath;
     }
 
-    public void calculatePathLength(double adjMatrix[][])
-    {
+    public void calculatePathLength(double[][] adjMatrix) {
         double length = 0;
-        for (int i = 0; i < path.size() - 1; i++)
-        {
+        for (int i = 0; i < path.size() - 1; i++) {
             length = length + adjMatrix[path.get(i)][path.get(i + 1)];
         }
         this.lengthOfPath = length;
