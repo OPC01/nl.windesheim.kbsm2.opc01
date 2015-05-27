a41f406b51d989bf9a6e98d9405ecfac621a4fe8
W7Home <W7Home@W7Home-PC>
2015-05-27 11:00:42 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/build/built-jar.properties
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/TekenPanel.java
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/TekenTest.java

---
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.windesheim.kbsm2.opc01.bpp;
+
+/**
+ *
+ * @author W7Home
+ */
+public class Main {
+    
+    public static void main (String [] args){
+        HoofdScherm2 main = new HoofdScherm2();
+        main.setTitle("Hoofdscherm");
+    }
+}
