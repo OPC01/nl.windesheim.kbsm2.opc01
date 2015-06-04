8a7f3851502adf7fb699aaa03390b5e646141b00
W7Home <W7Home@W7Home-PC>
2015-06-04 11:09:42 +0200

    Merge origin/master
    Conflicts:
    	nl.windesheim.kbsm2.opc01/build/built-jar.properties
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/TekenPanel.java
    	nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/TekenTest.java

--- nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/Main.java
+++ nl.windesheim.kbsm2.opc01/src/nl/windesheim/kbsm2/opc01/bpp/Main.java
@@ -14,5 +14,6 @@ public class Main {
     public static void main (String [] args){
         HoofdScherm2 main = new HoofdScherm2();
         main.setTitle("Hoofdscherm");
+        main.setVisible(true);
     }
 }
